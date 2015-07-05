a847595adb49707277c32e6bf38b144ad224bccc
kenankomah <kenankomah@live.co.uk>
2015-07-05 16:13:33 +0100

    Responsive
    Page is now responsive

--- default.php
+++ default.php
@@ -2,8 +2,9 @@
 
 <html>
 <head>
-<script src="http://code.jquery.com/jquery-latest.js" type="text/javascript"> </script> 
-<script src="hover.js" type="text/javascript"> </script> 
+<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no '/> 
+
+
 <link href="sections.css" type="text/css" rel="stylesheet">
 	<title>My Web Projects</title>
     <meta charset ="utf-8">
@@ -12,49 +13,54 @@
 
 <body style="background: url(images/bgnoise_lg.png) repeat left top;">
 
-<div id="header" height="230" width="930">
-	<img src="images/header.jpg"  height="230" width="935">
-	<h1 id='home'><i>My Web Projects</i></h1>
+<div id="header">
+	<img src="images/header.jpg">
+	<h1><i id='title'>My Web Projects</i></h1>
 </div>
 
-<div id="container">
-
+<center> 
+<div id="container" >
 <ul id="navmenu">  
-   <li><a href="index.html" id="round">Home</a></li>
-   <li><a href="">JavaScript</a><span class ="darrow"><!--&#9660; --></span>
+   <li><a href="index.html" id="round"><span class="navtext">Home</span></a></li>
+   <li><a href="#"><span class="navtext">JavaScript</span></a>
 		<ul class="sub1"> 
-			<li><a href="">HTML5 Games</a>
+			<li><a href="#"><span class="navtext">HTML5 Games</span></a>
 				<ul class="sub2"> 
-					<li><a href="games\shooter\shter.html" target="_blank">Shooter</a></li>
-					<li><a href="games\shooter\shter-ai.html" target="_blank">Shooter AI</a></li>
-					<li><a href="games\colourmatch\cm.html" target="_blank">Color Match</a></li>
-					<li><a href="games\snake\snake.html" target="_blank">Snake</a></li>
-					<li><a href="games\snake\snake-ai.html" target="_blank">Snake AI</a></li>
+					<li><a href="games/shooter/shter.html" target="_blank"><span class="navtext">Shooter</span></a></li>
+					<li><a href="games/shooter/shter-ai.html" target="_blank"><span class="navtext">Shooter AI</span></a></li>
+					<li><a href="games/colourmatch/cm.html" target="_blank"><span class="navtext">Color Match</span></a></li>
+					<li><a href="games/snake/snake.html" target="_blank"><span class="navtext">Snake</span></a></li>
+					<li><a href="games/snake/snake-ai.html" target="_blank"><span class="navtext">Snake AI</span></a></li>
 				</ul>
 			</li>
-			<li><a href="">Experiments</a>
+			<li><a href="#"><span class="navtext">Experiments</span></a>
 				<ul class="sub2"> 
-					<li><a href="experiments\random-walk\rw.html" target="_blank">Random Walk</a></li>
-					<li><a href="experiments\flock\t.html" target="_blank">Flock Simulation</a></li>
+					<li><a class='flockwalk' href="experiments/random-walk/rw.html" target="_blank"><span class="navtext"><span class="fontsize">Random Walk</span></span></a></li>
+					<li><a class='flockwalk' href="experiments/flock/t.html" target="_blank"><span class="fontsize">flock simulation</span></a></li>
 				</ul>
 			</li>
 		</ul>
    </li>
-   <li><a href="">CSS3</a>
+   <li><a href="#"><span class="navtext">CSS3</span></a>
 		<ul class="sub1"> 
-			<li><a href="css3\earth-sun-system\esms.html" target="_blank">Earth Sun System</a></li>
-			<li><a href="css3\language-tool\german.html" target="_blank"><span class="fontsize">language Tool</span></a></li>
+			<li><a href="css3/earth-sun-system/esms.html" target="_blank">Earth Sun System</a></li>
+			<li><a href="css3/language-tool/german.html" target="_blank"><span class="navtext">language Tool</span></a></li>
 		</ul>	
    </li>
-   <li><a href="">PHP & SQL</a><span class ="darrow"><!--&#9660; --></span>
+   <li><a href="#"><span class="navtext">PHP & SQL</span></a>
 		<ul class="sub1"> 
-			<li><a href="ajax\ajaxchat\index.php" target="_blank">AJAX Chat</a></li>
+			<li><a href="ajax/ajaxchat/index.php" target="_blank"><span class="navtext">AJAX Chat</span></a></li>
+		<!--	<li><a href="games/shooter/shter.html" target="_blank"><span class="navtext">Shooter</span></a></li> -->
 		</ul>
 	</li>
-	<li><a href="about.php">about</a></li>
-	<li><a href="contact.php" id="round1">Contact</a></li>
+	<li><a href="about.php"><span class="navtext">about</span></a></li>
+	<li><a href="contact.php" id="round1"><span class="navtext">Contact</span></a></li>
  </ul>
- </div>
 
+</div>
+</center> 
+ 
 </body> 
 </html>
+
+
