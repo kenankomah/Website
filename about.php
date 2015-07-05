1877a3cde8088365788d1bee354280ccb73cf67f
kenankomah <kenankomah@live.co.uk>
2015-07-05 16:09:46 +0100

    Responsive
    The page has been made responsive

--- about.php
+++ about.php
@@ -2,12 +2,13 @@
 <body>
 
 	<?php include 'default.php';?>
- 
-<div id="content">
-<h1> About Me</h1>
-<p> My name is Ken Ankomah, I first become interested in Web Development 2012 and I have spent quite a bit of time since then teaching myself to code and it has been a really fun adventure, and I look forward to learning a lot more.<br><br/> This site shows a collection of some of the projects that I have worked on and techniques that I've learnt since I embarked on this journey.<br><br/> I have some more projects in the pipeline which I will to add to the site in the near future, in the meantime take look around and enjoy.</p>
-
+<div id='container1' > 	
+	<div id="content">
+		<h1> About Me</h1>
+		<p> My name is Ken Ankomah, I first become interested in Web Development 2012 and I have spent quite a bit of time since then teaching myself to code and it has been a really fun adventure, and I look forward to learning a lot more.<br><br/> This site shows a collection of some of the projects that I have worked on and techniques that I've learnt since I embarked on this journey.<br><br/> I have some more projects in the pipeline which I will to add to the site in the near future, in the meantime take look around and enjoy.</p>
+	</div>
 </div>
+
 <div id="footer">
 	<div id='line'>
 		<ul id='foot'>
@@ -16,8 +17,8 @@
 			<li id='border'><a href="index.html"><span class='foottext'> Home </span></a></li>
 		</ul>
 	</div>
-</div>
-<div id='copy'><p> All Rights Reserved 2015 &copy Kenneth Ankomah. </p> </div>
+	<p id='copy'> All Rights Reserved 2015 &copy Kenneth Ankomah. </p>
+</div>                 
 
 </body>
 </html>
