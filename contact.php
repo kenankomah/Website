71c84857eb0ac9a92c04a3f279d7e29e459f5161
kenankomah <kenankomah@live.co.uk>
2015-07-05 16:11:55 +0100

    Responsive
    Page is now responsive

--- contact.php
+++ contact.php
@@ -2,14 +2,15 @@
 <body>
 
 	<?php include 'default.php';?>
-	
-<div id="content">
-<h1>Contact Details</h1>
-<p>My contact details are below, so please feel free to get in touch if you want to know more about myself and my projects or to let me know if you discover any bugs that I've missed. <br/><br/>
-Email: kenankomah@live.co.uk <br/><br/>
-Mobile Phone: 07506167046 </p>
-
+<div id='container1' > 	
+	<div id="content">
+		<h1>Contact Details</h1>
+		<p>My contact details are below, so please feel free to get in touch if you want to know more about myself and my projects or to let me know if you discover any bugs that I've missed. <br/><br/>
+		Email: kenankomah@live.co.uk <br/><br/>
+		Mobile Phone: 07506167046 </p>
+	</div>
 </div>
+
 <div id="footer">
 	<div id='line'>
 		<ul id='foot'>
@@ -18,8 +19,9 @@ Mobile Phone: 07506167046 </p>
 			<li id='border'><a href="index.html"><span class='foottext'> Home </span></a></li>
 		</ul>
 	</div>
-</div>
-<div id='copy'><p> All Rights Reserved 2015 &copy Kenneth Ankomah. </p> </div>
+	<p id='copy'> All Rights Reserved 2015 &copy Kenneth Ankomah. </p>
+</div>                 
+
 
 </body>
 </html>
